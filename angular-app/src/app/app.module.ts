5c501aa56cf8ecefeaadb4d3a792729c22e83a2b
d3bes <abdo.mohamed6319@gmail.com>
2023-08-21 01:13:10 +0300

    update routing-bootstrap

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
