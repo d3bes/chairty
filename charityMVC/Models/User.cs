c803b280ba118b586bc7243165dc5d582fd77ddf
d3bes <abdo.mohamed6319@gmail.com>
2023-08-23 23:16:34 +0300

    update user

--- charityMVC/Models/User.cs
+++ charityMVC/Models/User.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,27 +12,31 @@ namespace charityMVC.Models
         public string id { get;set;}
         public string phone { get; set;}
         public string birthDate { get; set;}
-        public int points { get; set;}
-        public string city { get; set;}
-        public string id_image { get; set;}
-        public string family_card_image { get; set;}
-        public string bank_account_number { get; set;}
-        public int children_count { get; set;}
-        public bool income_support { get; set;}
-        public bool disability { get; set;}
-        public string disability_proof { get; set;}
-        public bool widow { get; set;}
-        public bool elderly { get; set;}
+        public int? points { get; set;}
+        public string? city { get; set;}
+        public string? id_image { get; set;}
+        public string? family_card_image { get; set;}
+        public string? bank_account_number { get; set;}
+        public int? children_count { get; set;}
+        public bool? income_support { get; set;}
+        public bool? disability { get; set;}
+        public string? disability_proof { get; set;}
+        public bool? widow { get; set;}
+        public bool? elderly { get; set;}
 
-        public bool debt { get; set;}
-        public string debt_proof { get; set;}
-        public bool house_rent { get; set;}
-        public string rent_proof { get; set;}
-        public bool proxy { get; set;}
-        public string proxy_name { get; set;}
-        public string proxy_account_number { get; set;}
+        public bool? debt { get; set;}
+        public string? debt_proof { get; set;}
+        public bool? house_rent { get; set;}
+        public string? rent_proof { get; set;}
+        public bool? proxy { get; set;}
+        public string? proxy_name { get; set;}
+        public string? proxy_account_number { get; set;}
 
-        public bool isDeleted {get;set;}
+        public bool? isDeleted {get;set;}
+        [ForeignKey("clerk")]
+        public string clerkId { get; set;}
+
+        public Clerk clerk { get; set;}
 
 
 
