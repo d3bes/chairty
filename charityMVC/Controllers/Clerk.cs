025a912b92807baa71293efb8237179430dd7440
d3bes <abdo.mohamed6319@gmail.com>
2023-08-28 07:38:16 +0300

    update clerk

--- charityMVC/Controllers/Clerk.cs
+++ charityMVC/Controllers/Clerk.cs
@@ -31,6 +31,9 @@ namespace charityMVC.Controllers
 
         public async Task<IActionResult> ClerkProfil(string id)
         {
+
+            try
+            {
             var ClerkProfil = new ClerkProfileVM();
             Models.Clerk clerk = await _clerkRepo.GetClerkById(id);
             var cityUsers = await _userRepo.GetCityUsers(clerk.city);
@@ -39,6 +42,19 @@ namespace charityMVC.Controllers
             
 
             return View("cityUsers", ClerkProfil);
+            }
+             catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "An error occurred in the Review action.");
+                        
+                        TempData["ErrorMessage"] = "عذرا لقد وقع خطا غير مقصود اذا تكرر عليك التواصل مع المبرمج !";
+                        
+                        if(ClerkProfil != null )
+                        return View("cityUsers", ClerkProfil);
+                        else
+                        return View("cityUsers");
+                        
+                    }
         }
 
         public async Task<IActionResult> Review(string id)
@@ -47,6 +63,58 @@ namespace charityMVC.Controllers
 
             return View("review",user);
         }
+
+        
+        [HttpPost]
+        public async Task<IActionResult> UpdatePersonslData( PersonalData personalData, string id)
+        {
+
+                    try{     
+                            
+                                var user = await _userRepo.GetUserById(id);
+                                if(user == null)
+                                {
+                                    user = await _userRepo.GetUserById(personalData.id);
+                                }
+                                            
+                                user.fullName = personalData.fullName;
+                                user.phone = "966"+personalData.phone;
+                                user.birthDate = personalData.birthDate;
+                                user.city = personalData.city;
+                                user.fullAddress = personalData.fullAddress;
+                                user.bank_account_number = personalData.bank_account_number;
+                                user.children_count = personalData.children_count;
+                                user._proxy_account_number = personalData._proxy_account_number;
+                                user._proxy_name = personalData._proxy_name;
+                                user.proxy = personalData.proxy;
+                                user.elderly= personalData.elderly;
+                                user.widow= personalData.widow;
+                                user.income_support = personalData.income_support;
+                              
+                                var points =  await _userRepo.GetPoints(user);
+                                user.points = points;
+
+                                await _userRepo.Update(user);
+
+                                // Redirect to the Index action of the HomeController
+                      return RedirectToAction("Review", "Clerk", new { id = personalData.id });
+                        // // If ModelState is not valid, return to the view with validation errors
+                        // return View("register",user);
+
+                    }    
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "An error occurred in the Review action.");
+                        
+                        TempData["ErrorMessage"] = "عذرا لقد وقع خطا غير مقصود اذا تكرر عليك التواصل مع المبرمج !";
+                        
+                        return RedirectToAction("Review", "Clerk", new { id = id });
+                    }
+
+                            
+
+        }
+
 
     }
 
