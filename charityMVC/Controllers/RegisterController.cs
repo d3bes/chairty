dcfe5b73c9654d7eba593d2c36a52bf788b3c047
d3bes <abdo.mohamed6319@gmail.com>
2023-08-28 07:39:23 +0300

    add Login functionality

--- charityMVC/Controllers/RegisterController.cs
+++ charityMVC/Controllers/RegisterController.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using charityMVC.Models;
 using charityMVC.Repository;
 using charityMVC.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace charityMVC.Controllers
@@ -28,40 +32,101 @@ namespace charityMVC.Controllers
         { 
             return View("register");
         }
+        public IActionResult Login()
+        { 
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(Account account)
+        {
+        
+        try 
+        {
+          if(ModelState.IsValid)
+          {
+            if(_userRepo.Found(account.id, account.password))
+            {
+                //get role
+               Roles role = await _userRepo.GetRoles(account.id);
+                User accountModel =   _userRepo.Find(account.id, account.password);
+               
+                   ClaimsIdentity identity = 
+                    new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+
+                identity.AddClaim(new Claim("id",account.id.ToString()));
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, account.id.ToString()));
+                
+                identity.AddClaim(new Claim(ClaimTypes.Role, role.Role));
+
+                 ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+                 
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,principal);
+
+                 return RedirectToAction("GetUserProfile", "UserProfile");     
+            }
+            else 
+            return View(account);
+          }
+               return View();
+
+        }
+         catch (Exception ex)
+                     {
+                        _logger.LogError(ex, "An error occurred in the Review action.");
+                        
+                        TempData["ErrorMessage"] = "عذرا لقد وقع خطا غير مقصود اذا تكرر اكثر عليك التواصل مع المبرمج !";
+                    
+                      return View(); 
+
+                       }
+
+
+        }
+
         [HttpPost]
         public async Task<IActionResult> Upload(IFormFile id_image, IFormFile family_card_image, IFormFile disability_proof,
                                     IFormFile debt_proof, IFormFile rent_proof, User user )
         {
-                  
-                        // List of form files to be uploaded
-                        List<IFormFile> formFiles = new List<IFormFile> { id_image, family_card_image, disability_proof, debt_proof, rent_proof };
+            try 
+            {
 
-                        foreach (var file in formFiles)
-                        {
-                            if (file != null)
-                            {
-                                string UploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "userImages");
-                                string fileName = Guid.NewGuid().ToString() + "_" + file.FileName;
-                                string filePath = Path.Combine(UploadPath, fileName);
+                if(ModelState.IsValid)
+                {
+                        Roles role = new Roles
+                                {
+                                    id = user.id,
+                                    Role = "user"
+                                };
+                                 await _userRepo.AddRole(role);
+
                                 
-                                // Upload the image file
-                                _userRepo.Uploadimage(file, filePath);
-                                
-                                // Assign the uploaded file path to the corresponding property in the user object
-                                if (file == id_image) user.id_image = Path.Combine("\\userImages", fileName);
-                                else if (file == family_card_image) user.family_card_image = Path.Combine("\\userImages", fileName);
-                                else if (file == disability_proof) user.disability_proof = Path.Combine("\\userImages", fileName);
-                                else if (file == debt_proof) user.debt_proof = Path.Combine("\\userImages", fileName);
-                                else if (file == rent_proof) user.rent_proof = Path.Combine("\\userImages", fileName);
-                            }
-                        }
-                        
-                                // Calculate user points and add the user
+                                await _userRepo.UploadAllFiles( id_image, family_card_image, disability_proof, debt_proof, rent_proof ,user);
+                                  // Calculate user points and add the user
                                 user.points = await _userRepo.GetPoints(user);
                                 await _userRepo.AddUser(user);
+                              
+                      
+                        
+                               
+                                        // Redirect to the Index action of the HomeController
+                        return View("Login");
+                        }
+                        else
+                        return View("register",user); 
+                    }
 
-                                // Redirect to the Index action of the HomeController
-                 return RedirectToAction("GetUserProfile", "UserProfile", new { id = user.id });
+                    catch (Exception ex)
+                     {
+                        _logger.LogError(ex, "An error occurred in the Review action.");
+                        
+                        TempData["ErrorMessage"] = "عذرا لقد وقع خطا غير مقصود اذا تكرر اكثر عليك التواصل مع المبرمج !";
+                    
+                      return View("register",user); 
+
+                       }
 
 
                         // // If ModelState is not valid, return to the view with validation errors
@@ -73,48 +138,13 @@ namespace charityMVC.Controllers
         [HttpPost]
         public async Task<IActionResult> UpdatePersonslData( PersonalData personalData )
         {
-
- #region edited
-                        // List<IFormFile> formFiles = new List<IFormFile> ();
-
-                        // if(OldUser.id_image != user.id_image ) formFiles.Add(id_image);
-                        // if(OldUser.family_card_image != user.family_card_image ) formFiles.Add(family_card_image);
-                        // if(OldUser.disability_proof != user.disability_proof ) formFiles.Add(disability_proof);
-                        // if(OldUser.debt_proof != user.debt_proof ) formFiles.Add(debt_proof);
-                        // if(OldUser.rent_proof != user.rent_proof ) formFiles.Add(rent_proof);
-
-                        // if(formFiles != null)
-                        // {
-                        //     foreach (var file in formFiles)
-                        //     {
-                        //         if (file != null)
-                        //         {
-                        //             string UploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "userImages");
-                        //             string fileName = Guid.NewGuid().ToString() + "_" + file.FileName;
-                        //             string filePath = Path.Combine(UploadPath, fileName);
-                                    
-                        //             // Upload the image file
-                        //             _userRepo.Uploadimage(file, filePath);
-                                    
-                        //             // Assign the uploaded file path to the corresponding property in the user object
-                        //             if (file == id_image) user.id_image = Path.Combine("\\userImages", fileName);
-                        //             else if (file == family_card_image) user.family_card_image = Path.Combine("\\userImages", fileName);
-                        //             else if (file == disability_proof) user.disability_proof = Path.Combine("\\userImages", fileName);
-                        //             else if (file == debt_proof) user.debt_proof = Path.Combine("\\userImages", fileName);
-                        //             else if (file == rent_proof) user.rent_proof = Path.Combine("\\userImages", fileName);
-                        //         }
-                        //     }
-                        // }
-                        
-                                // Calculate user points and add the user
-                                // user.points = await _userRepo.GetPoints(user);
-
-        #endregion
+                    try
+                    {
                                
                              var user = await _userRepo.GetUserById(personalData.id);
                                 
                                 user.fullName = personalData.fullName;
-                                user.phone = personalData.phone;
+                                user.phone = "966"+personalData.phone;
                                 user.birthDate = personalData.birthDate;
                                 user.city = personalData.city;
                                 user.fullAddress = personalData.fullAddress;
@@ -133,13 +163,24 @@ namespace charityMVC.Controllers
                                 await _userRepo.Update(user);
 
                                 // Redirect to the Index action of the HomeController
-                return RedirectToAction("GetUserProfile", "UserProfile", new { id = user.id });
-
-
+                 return RedirectToAction("GetUserProfile", "UserProfile");
                         // // If ModelState is not valid, return to the view with validation errors
                         // return View("register",user);
+                    }
+                      catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "An error occurred in the Review action.");
+                        
+                        TempData["ErrorMessage"] = "عذرا لقد وقع خطا غير مقصود اذا تكرر اكثر عليك التواصل مع المبرمج !";
+                    
+                        return RedirectToAction("GetUserProfile", "UserProfile");
+
+                    }
 
         }
+
+
+        
 
      
     }
